c579103c425895e2a52931869b788310b36cbddf
RandallCorellaCastillo <randallcorella@estudiantec.cr>
2024-09-28 16:58:09 -0600

    comments

--- hardos/src/main/java/itcr/model/Assembler.java
+++ hardos/src/main/java/itcr/model/Assembler.java
@@ -5,22 +5,25 @@ import java.util.List;
 
 public class Assembler {
 
-  private static final String MOV_REGEX = "^MOV\\s+([A-D]X)\\s*,\\s*(([A-D]X)|\\d+)$";
-  private static final String LOAD_REGEX = "^LOAD\\s+([A-D]X)\\s*$";
-  private static final String STORE_REGEX = "^STORE\\s+([A-D]X)\\s*$";
-  private static final String ADD_REGEX = "^ADD\\s+([A-D]X)\\s*$";
-  private static final String SUB_REGEX = "^SUB\\s+([A-D]X)\\s*$";
-  private static final String INC_REGEX = "^INC\\s+([A-D]X)\\s*$|INC";
-  private static final String DEC_REGEX = "^DEC\\s+([A-D]X)\\s*$|DEC";
-  private static final String SWAP_REGEX = "^SWAP\\s+([A-D]X)\\s*,\\s*([A-D]X)\\s*$";
-  private static final String INT_REGEX = "^INT\\s+\\_[0-9][0-9]H\\s*$";
-  private static final String JMP_REGEX = "^JMP\\s*[+\\-]?\\d+\\s*$";
-  private static final String CMP_REGEX = "^CMP\\s+([A-D]X)\\s*,\\s*([A-D]X)\\s*$";
-  private static final String JE_REGEX = "^JE\\s*[+\\-]?\\d+\\s*$";
-  private static final String JNE_REGEX = "^JNE\\s*[+\\-]?\\d+\\s*$";
-  private static final String PARAM_REGEX = "^PARAM\\s+(-?\\d+\\s*(,\\s-?\\d+\\s*){0,2})$";
-  private static final String PUSH_REGEX = "^PUSH\\s+([A-D]X)\\s*$";
-  private static final String POP_REGEX = "^POP\\s+([A-D]X)\\s*$";
+  private static final String MOV_REGEX = "^MOV\\s+([A-D]X)\\s*,\\s*(([A-D]X)|\\d+)(\\s*//.*)?$";
+  private static final String LOAD_REGEX = "^LOAD\\s+([A-D]X)(\\s*//.*)?$";
+  private static final String STORE_REGEX = "^STORE\\s+([A-D]X)(\\s*//.*)?$";
+  private static final String ADD_REGEX = "^ADD\\s+([A-D]X)(\\s*//.*)?$";
+  private static final String SUB_REGEX = "^SUB\\s+([A-D]X)(\\s*//.*)?$";
+  private static final String INC_REGEX = "^INC\\s+([A-D]X)(\\s*//.*)?$|^INC(\\s*//.*)?$";
+  private static final String DEC_REGEX = "^DEC\\s+([A-D]X)(\\s*//.*)?$|^DEC(\\s*//.*)?$";
+  private static final String SWAP_REGEX = "^SWAP\\s+([A-D]X)\\s*,\\s*([A-D]X)(\\s*//.*)?$";
+  private static final String INT_REGEX = "^INT\\s+\\_[0-9][0-9]H(\\s*//.*)?$";
+  private static final String JMP_REGEX = "^JMP\\s*[+\\-]?\\d+(\\s*//.*)?$";
+  private static final String CMP_REGEX = "^CMP\\s+([A-D]X)\\s*,\\s*([A-D]X)(\\s*//.*)?$";
+  private static final String JE_REGEX = "^JE\\s*[+\\-]?\\d+(\\s*//.*)?$";
+  private static final String JNE_REGEX = "^JNE\\s*[+\\-]?\\d+(\\s*//.*)?$";
+  private static final String PARAM_REGEX = "^PARAM\\s+(-?\\d+\\s*(,\\s-?\\d+\\s*){0,2})(\\s*//.*)?$";
+  private static final String PUSH_REGEX = "^PUSH\\s+([A-D]X)(\\s*//.*)?$";
+  private static final String POP_REGEX = "^POP\\s+([A-D]X)(\\s*//.*)?$";
+  private static final String COMMENT_REGEX = "^(//.*)$";
+
+  
 
   private static String validateInstruction(String instruction, int lineNumber) {
     if (instruction.matches(MOV_REGEX)) return null;
@@ -39,6 +42,7 @@ public class Assembler {
     if (instruction.matches(PARAM_REGEX)) return null;
     if (instruction.matches(PUSH_REGEX)) return null;
     if (instruction.matches(POP_REGEX)) return null;
+    if (instruction.matches(COMMENT_REGEX)) return null;
 
     // If we reach this point, the instruction is not recognized
     return String.format("Error en línea %d: Instrucción no reconocida '%s'", lineNumber, instruction);
